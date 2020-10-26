e0ca06da0f9da5e0f450230556fd6fc5783913fd
Liam Nelski <ljnelski@gmail.com>
2020-10-26 12:06:18 -0400

    Restructured current files to be as close to model view controller setup with out adding any new folder/files

--- server/config/app.js
+++ server/config/app.js
@@ -1,15 +1,17 @@
+// Node dependencies
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+// Sets up the routers
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
 
 let app = express();
 
-// view engine setup
+// views setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -17,9 +19,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
+// route setup
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
